66a798609a7a06cf43a0ae5f2450f66b0e6b2e74
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-04-09 19:38:43 +0200

    Updates

---
+++ precache-manifest.c1630f159a4a8fa4f0985067990925c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cbf0167a0ffd0953b4d",
+    "url": "/Drag-Drop_In_React/static/js/runtime~main.639355d8.js"
+  },
+  {
+    "revision": "851b7289a2fff9861c21",
+    "url": "/Drag-Drop_In_React/static/js/main.0ea1908a.chunk.js"
+  },
+  {
+    "revision": "fc1a04f5442fc70ce71f",
+    "url": "/Drag-Drop_In_React/static/js/2.f9075ab4.chunk.js"
+  },
+  {
+    "revision": "851b7289a2fff9861c21",
+    "url": "/Drag-Drop_In_React/static/css/main.a5c79fd5.chunk.css"
+  },
+  {
+    "revision": "fc68ed6f610e614fd32551ed8c8a2891",
+    "url": "/Drag-Drop_In_React/index.html"
+  }
+];
